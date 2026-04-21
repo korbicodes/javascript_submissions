35415f700ae736dda8812cb55a1540b701d0e021
eKorbi <bedriekorbi@gmail.com>
2026-04-21 13:31:08 +0200

    variable misspell

--- review/looping-obj.js
+++ review/looping-obj.js
@@ -63,7 +63,7 @@ const entries = Object.entries(openingHours)
 console.log(entries);
 
 
-for (const [key, { open, close }] of entires) {
+for (const [key, { open, close }] of entries) {
     console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
 
