2228432b55058fbe75775d0332054354e5f5340d
eKorbi <bedriekorbi@gmail.com>
2026-07-04 19:14:56 +0200

    array grouping, new account property added

--- projects/project2/script.js
+++ projects/project2/script.js
@@ -10,6 +10,7 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  type: 'premium'
 };
 
 const account2 = {
@@ -17,6 +18,8 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  type: 'standard'
+
 };
 
 const account3 = {
@@ -24,6 +27,8 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  type: 'premium'
+
 };
 
 const account4 = {
@@ -31,6 +36,8 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  type: 'basic'
+
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -498,4 +505,26 @@ movements.sort((a,b)=>a-b)
 
 //descending
 movements.sort((a,b)=>b-a)
+console.log(movements);
+
+
+
+//array grouping - group values in array based in condition
 console.log(movements)
+const groupedMovements = Object.groupBy(movements, movement => movement > 0 ? 'deposits' : 'withdrawals');
+console.log(groupedMovements);
+
+
+const grupedByActivity = Object.groupBy(accounts, account => {
+  const movementCount = account.movements.length;
+  if (movementCount > 8) return 'very active'
+  if(movementCount >= 4) return 'active'
+  if(movementCount >= 1) return 'moderate'
+  return 'inactive'
+
+
+});
+
+// const groupedAccounts = Object.groupBy(accounts, account => account.type);
+const groupedAccounts = Object.groupBy(accounts, ({type})=>type)
+console.log(groupedAccounts);
